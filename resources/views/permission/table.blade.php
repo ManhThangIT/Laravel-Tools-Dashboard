4b793f30e6ea1998968aa682c6aabf60baddfcab
ManhThangIT <xichemtrongtim@gmail.com>
2019-07-06 00:27:13 +0700

    Tools Manager Admin

---
+++ resources/views/permission/table.blade.php
@@ -0,0 +1,28 @@
+<div class="table-responsive">
+    <table class="table" id="users-table">
+        <thead>
+            <tr>
+                <th>MENU</th>
+        <th>VIEW</th>
+        <th>EDIT</th>
+        <th>UPDATE</th>
+        <th>DELETE</th>
+            </tr>
+        </thead>
+        <tbody>
+            <td>User</td>
+            <td>
+                <input type="checkbox">
+            </td>
+            <td>
+                <input type="checkbox">
+            </td>
+            <td>
+                <input type="checkbox">
+            </td>
+            <td>
+                <input type="checkbox">
+            </td>
+        </tbody>
+    </table>
+</div>
